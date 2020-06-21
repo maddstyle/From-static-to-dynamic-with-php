9bf43003beead1c3b268a854d6b8482e59481571
Michaela DeCamara <michaela.decamara@gmail.com>
2020-06-21 00:44:57 -0400

    partials created

--- index.php
+++ index.php
@@ -1,59 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Nightfury</title>
+<?php include 'partials/header.php' ?>
 
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&amp;subset=latin,latin-ext">
-	<link rel="stylesheet" href="css/font-awesome.css">
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/style.css">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<script src="js/jquery-2.1.1.min.js"></script>
-	<!--[if lt IE 9]>
-		<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<![endif]-->
-</head>
-<body>
-
-	<header>
-		<div class="contact-bar">
-			<div class="container">
-				<ul class="menu personal">
-					<li><i class="fa fa-phone"></i><a href="tel:+421900111222">+421 900 111 222</a></li>
-					<li><i class="fa fa-envelope"></i><a href="mailto:email@mailinator.com">email@mailinator.com</a></li>
-				</ul>
-				<ul class="menu social">
-					<li><a href="#" class="social-icon"><i class="fa fa-github"></i></a></li>
-					<li><a href="#" class="social-icon"><i class="fa fa-twitter"></i></a></li>
-					<li><a href="#" class="social-icon"><i class="fa fa-facebook"></i></a></li>
-					<li><a href="#" class="social-icon"><i class="fa fa-linkedin"></i></a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="nav-bar">
-			<div class="container">
-				<h1 class="logo">
-					<a href="#">
-						<strong>NIGHT</strong>FURY<i class="fa fa-fire"></i>
-					</a>
-				</h1>
-
-				<nav class="group">
-					<ul class="menu navigation">
-						<li><a href="index.html"><i class="fa fa-shield fa-2x"></i> Domov</a></li>
-						<li class="selected"><a href="portfolio.html"><i class="fa fa-leaf fa-2x"></i> Portfolio</a></li>
-						<li><a href="about.html"><i class="fa fa-bolt fa-2x"></i> O nás</a></li>
-						<li><a href="contact.html"><i class="fa fa-trophy fa-2x"></i> Kontakt</a></li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	</header>
-
-	<main>
 		<article>
 			<header class="post-header">
 				<div class="container">
@@ -67,10 +13,10 @@
 
 					<ul class="menu controls">
 						<li data-from="left" class="selected"><a href="#vsetko">Všetko</a></li>
-						<li data-from="right"><a href="web.html">Web</a></li>
-						<li data-from="left"><a href="branding.html">Branding</a></li>
-						<li data-from="right"><a href="fotografia.html">Fotografia</a></li>
-						<li data-from="left"><a href="video.html">Video</a></li>
+						<li data-from="right"><a href="web.php">Web</a></li>
+						<li data-from="left"><a href="branding.php">Branding</a></li>
+						<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
+						<li data-from="left"><a href="video.php">Video</a></li>
 					</ul>
 
 				</div>
@@ -124,10 +70,10 @@
 				Návrh a design pripravil <a href="mailto:tomas@cloudfender.com">tomas@cloudfender.com</a>
 			</p>
 			<ul class="menu nav-footer">
-				<li><a href="index.html">Domov</a></li>
-				<li><a href="portfolio.html">Portfólio</a></li>
-				<li><a href="about.html">O nás</a></li>
-				<li><a href="contact.html">Kontakt</a></li>
+				<li><a href="index.php">Domov</a></li>
+				<li><a href="portfolio.php">Portfólio</a></li>
+				<li><a href="about.php">O nás</a></li>
+				<li><a href="contact.php">Kontakt</a></li>
 			</ul>
 		</div>
 	</footer>
@@ -136,4 +82,4 @@
 	<script src="js/lightbox.js"></script>
 
 </body>
-</html>
+<?php include 'partials/footer.php'; ?>
---
+++ partials/footer.php
@@ -0,0 +1,22 @@
+<footer>
+		<div class="container">
+			<p class="logo">
+				<strong>NIGHT</strong>FURY<i class="fa fa-fire"></i>
+			</p>
+			<p class="author">
+				Návrh a design pripravil <a href="mailto:tomas@cloudfender.com">tomas@cloudfender.com</a>
+			</p>
+			<ul class="menu nav-footer">
+				<li><a href="index.html">Domov</a></li>
+				<li><a href="portfolio.html">Portfólio</a></li>
+				<li><a href="about.html">O nás</a></li>
+				<li><a href="contact.html">Kontakt</a></li>
+			</ul>
+		</div>
+	</footer>
+
+	<!-- scripts -->
+	<script src="js/lightbox.js"></script>
+
+</body>
+</html>
